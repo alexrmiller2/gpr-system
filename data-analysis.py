0fe72e856d086ca90772e1d2d6fc080cd6bb974a
alexrmiller2 <alexrmiller2@gmail.com>
2025-03-28 16:00:21 -0400

    changes to data script

--- data-analysis.py
+++ data-analysis.py
@@ -4,8 +4,10 @@ import pandas as pd
 # Fetch historical data
 data = yf.download('AAPL', start='2023-01-01', end='2024-03-27')
 
+priceData = data['Close'].values
+
 fileName = "GBPUSD_M1.csv"
 data2 = pd.read_csv(fileName)
 
-print(data.columns)
-print(data2.columns)
+print(priceData.shape)
+
